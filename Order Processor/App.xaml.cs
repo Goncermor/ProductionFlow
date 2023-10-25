2ea0678b448c562b2467c3fe768e97e0a23a1ee0
Goncermor <goncalocerto9morais@gmail.com>
2023-10-25 16:40:49 +0100

    Adicionar arquivos de projeto.

---
+++ Order Processor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Order_Processor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
